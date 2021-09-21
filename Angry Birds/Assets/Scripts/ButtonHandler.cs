458faed7f057f53ca5507d41d6b4fcda1c226d0f
FahriDarmawan <44422245+FahriDarmawan@users.noreply.github.com>
2021-09-22 03:01:46 +0700

    Angry Birds

---
+++ Angry Birds/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void RetryGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GoToSecondLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+				Application.Quit();
+        #endif
+    }
+}
---
+++ Angry Birds/Assets/Scripts/Owl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Owl : Bird
+{
+    private bool explode = false;
+
+    [SerializeField] private GameObject _explosion;
+
+    public void Boom()
+    {
+        if(State == BirdState.Thrown && !explode)
+        {
+            GameObject obj;
+            obj = Instantiate(_explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            obj.SetActive(true);
+            explode = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Boom();
+    }
+}
